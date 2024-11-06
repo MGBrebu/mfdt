68a0413092f22671feb6c8fa56f372cfa91341fe
Mariko <hmmarik@gmail.com>
2024-11-06 20:27:50 +0000

    added virustotal check function

--- mfdt-cli.py
+++ mfdt-cli.py
@@ -2,7 +2,6 @@ import hashlib
 import os
 import requests
 
-
 def hash_file(file_path, algorithm='md5'):
     with open(file_path, 'rb') as f:
         digest = hashlib.file_digest(f, algorithm)
@@ -23,6 +22,17 @@ def check_nsrl_online(hash_value, hash_algorithm='md5'):
     if response.status_code == 200:
         return response.json()  # Returns details if the hash is found
     return None
+
+def check_virustotal(hash, api_key):
+    url = f"https://www.virustotal.com/api/v3/files/{hash}"
+    headers = {"x-apikey": api_key}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data', {}).get('attributes', {}).get('last_analysis_stats', {})
+    return None
+
+#----------------------------------------------
 
 file_path = './test-files/unknown_benign2.docx'
 file_hash = hash_file(file_path, 'sha256')
@@ -50,3 +60,10 @@ file_path = './test-files/Update.exe'
 file_hash = hash_file(file_path, 'sha256')
 print("File hash:", file_hash)
 print(check_nsrl_online(file_hash, 'sha256'))
+
+print("---------------------------------")
+
+file_path = './test-files/WizTree.exe'
+file_hash = hash_file(file_path, 'sha256')
+api_key = 'ef395087293e63f72a7838f86ee73431166b2e87fc8d225b1b7a8dcd007b191d'
+print(check_virustotal(file_hash, api_key))
