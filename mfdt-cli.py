4583da8cbd499496274134046c3f6a0197628ed0
Mariko <hmmarik@gmail.com>
2024-11-06 18:37:47 +0000

    added venv to gitignore compressed nsrl functions to one function hash_directory now returns a dict of path / hash

--- mfdt-cli.py
+++ mfdt-cli.py
@@ -1,5 +1,7 @@
 import hashlib
 import os
+import requests
+
 
 def hash_file(file_path, algorithm='md5'):
     with open(file_path, 'rb') as f:
@@ -12,6 +14,13 @@ def hash_directory(directory_path, algorithm='md5'):
             file_path = os.path.join(root, file)
             file_hash = hash_file(file_path, algorithm)
             print(file_path, " = ", file_hash)
+            
+def check_nsrl_online(hash_value):
+    url = f"https://hashlookup.circl.lu/api/hash/{hash_value}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()  # Returns details if the hash is found
+    return None
 
 file_path = './test-files/unknown_benign2.docx'
 file_hash = hash_file(file_path, 'sha256')
