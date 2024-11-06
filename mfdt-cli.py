3d93367baf31140526c51d6a6913aa342b1063b3
Mariko <hmmarik@gmail.com>
2024-11-06 18:38:57 +0000

    compressed nsrl functions to one function hash_directory() now returns a dict of path / hash

--- mfdt-cli.py
+++ mfdt-cli.py
@@ -9,14 +9,16 @@ def hash_file(file_path, algorithm='md5'):
     return digest.hexdigest()
 
 def hash_directory(directory_path, algorithm='md5'):
+    hash_values = {}
     for root, dirs, files in os.walk(directory_path):
         for file in files:
             file_path = os.path.join(root, file)
             file_hash = hash_file(file_path, algorithm)
-            print(file_path, " = ", file_hash)
+            hash_values[file_path] = file_hash
+    return hash_values
             
-def check_nsrl_online(hash_value):
-    url = f"https://hashlookup.circl.lu/api/hash/{hash_value}"
+def check_nsrl_online(hash_value, hash_algorithm='md5'):
+    url = f"https://hashlookup.circl.lu/lookup/{hash_algorithm}/{hash_value}"
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()  # Returns details if the hash is found
@@ -28,5 +30,23 @@ print("File hash:", file_hash)
 
 print("---------------------------------")
 
-directory_path = './test-files'
-hash_directory(directory_path, 'sha256')
+directory_path = 'test-files'
+for file_path, file_hash in hash_directory(directory_path, 'sha256').items():
+    print(file_path, " = ", file_hash)
+
+print("---------------------------------")
+
+file_path = './test-files/Update.exe'
+file_hash = hash_file(file_path, 'md5')
+print("File hash:", file_hash)
+print(check_nsrl_online(file_hash))
+
+file_path = './test-files/Update.exe'
+file_hash = hash_file(file_path, 'sha1')
+print("File hash:", file_hash)
+print(check_nsrl_online(file_hash, 'sha1'))
+
+file_path = './test-files/Update.exe'
+file_hash = hash_file(file_path, 'sha256')
+print("File hash:", file_hash)
+print(check_nsrl_online(file_hash, 'sha256'))
