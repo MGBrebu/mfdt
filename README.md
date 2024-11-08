c1796d2a97c354ede5c364cd603e16671ad13619
MGBrebu <mgbrebu@proton.me>
2024-11-08 13:52:20 +0000

    update .gitignore add better logging to functions, indentation

--- README.md
+++ README.md
@@ -5,9 +5,7 @@
 
 ## Overview
 <p align="justify">
-MFDT is a CLI/GUI tool that allows users to check a given directory or file system for malicious files. The tool calculates the hash for all provided files, compares the hashes against the <a href="https://www.nist.gov/itl/ssd/software-quality-group/national-software-reference-library-nsrl">NIST NSRL</a>, runs all remaining unknown files through <a href="https://www.virustotal.com/">VirusTotal</a>, and generates a report for the user.
-
-The tool includes an optional GUI for user convenience, though the tool is fully usable in the command line.
+MFDT is a CLI tool that allows users to check a given directory or file system for malicious files. The tool calculates the hash for all provided files, compares the hashes against the <a href="https://www.nist.gov/itl/ssd/software-quality-group/national-software-reference-library-nsrl">NIST NSRL</a>, runs all remaining unknown files through <a href="https://www.virustotal.com/">VirusTotal</a>, and generates a report for the user.
 </p>
 
 ## Usage
--- mfdt-cli.py
+++ mfdt-cli.py
@@ -6,48 +6,48 @@ import argparse
 
 # Hashes a single file given a file path and hash algorithm, defaults to MD5, returns file's hash value
 def hash_file(file_path, algorithm='md5'):
-    print(f"Calculating '{file_path}'...")
+    print(f"\t\tCalculating '{file_path}'...")
     with open(file_path, 'rb') as f:
         digest = hashlib.file_digest(f, algorithm)
-    print(f"File hash calculated: '{digest.hexdigest()}'")
+    print(f"\t\tFile hash calculated: '{digest.hexdigest()}'")
     return digest.hexdigest()
 
 # Hashes multiple files given a directory path and hash algorithm, defaults to MD5, returns dictionary of file paths and hash values
 # Calls hash_file() for each file in the directory
 def hash_directory(directory_path, algorithm='md5'):
-    print(f"Hashing files in '{directory_path}'...")
+    print(f"\t- Hashing files in '{directory_path}'...")
     hash_values = {}
     for root, dirs, files in os.walk(directory_path):
         for file in files:
             file_path = os.path.join(root, file)
             file_hash = hash_file(file_path, algorithm)
             hash_values[file_path] = file_hash
-    print("# Hashing directory complete")
+    print(f"\t# Hashing directory complete")
     return hash_values
            
 # Checks a hash value on CIRCL hash lookup service, returns the result if the hash is known, otherwise returns None
 def check_circl_hashlookup(hash_value, hash_algorithm='md5'):
-    print(f"Checking '{hash_value}' on CIRCL hash lookup...")
+    print(f"\t- Checking '{hash_value}' on CIRCL hash lookup...")
     url = f"https://hashlookup.circl.lu/lookup/{hash_algorithm}/{hash_value}"
     response = requests.get(url)
     if response.status_code == 200:
-        print("Hash lookup complete, file is known")
+        print("\t# Hash lookup complete, file is known")
         return response.json()
-    print("Hash lookup complete, file is unknown")
+    print("\t# Hash lookup complete, file is unknown")
     return None
 
 # Checks a hash value on VirusTotal, returns the result if the check is successful, otherwise returns None
 # NB: Does not check if the file is safe, simply returns the full response
 def check_virustotal(hash, api_key):
-    print(f"Checking '{hash}' on VirusTotal...")
+    print(f"\t- Checking '{hash}' on VirusTotal...")
     url = f"https://www.virustotal.com/api/v3/files/{hash}"
     headers = {"x-apikey": api_key}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
-        print("VirusTotal lookup complete.")
+        print("\t# VirusTotal lookup complete.")
         return data.get('data', {}).get('attributes', {}).get('last_analysis_stats', {})
-    print("VirusTotal lookup failed.")
+    print("\t# VirusTotal lookup failed.")
     return None
 
 # Classifies a file as malicious, safe, or unknown given a file path, hash value, and API key for VirusTotal
@@ -64,13 +64,12 @@ def classify_file(file_path, hash, api_key):
     circl_result = check_circl_hashlookup(hash)
     if circl_result:
         circl_malicous = circl_result.get("KnownMalicious", False)
-        circl_found = circl_result.get("Found", False)
 
     if circl_result:
         if circl_malicous:
             classification = "Malicious"
             file_details['circl'] = "Known Malicious"
-        elif circl_found:
+        else:
             classification = "Safe"
             file_details['circl'] = "Known Safe"
     else:
@@ -92,6 +91,7 @@ def classify_file(file_path, hash, api_key):
 # Uses classify_file() to classify all files in a directory given a directory path and an API key for VirusTotal
 # Returns a report dictionary with lists of malicious, safe, and unknown files and their details
 def classify_directory(directory_path, api_key):
+    print(f"- Classifying files in '{directory_path}'...")
     report = {
         "malicious": [
         ],
@@ -104,10 +104,12 @@ def classify_directory(directory_path, api_key):
     for file_path, hash in hash_values.items():
         classification, file_details = classify_file(file_path, hash, api_key)
         report[classification.lower()].append(file_details)
+    print("# Classification complete")
     return report
 
 # Generates a report textfile given a report dictionary and a directory path to save the report
 def generate_report(report, report_dir):
+    print(f"- Generating report...")
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     if not os.path.exists(report_dir):
         os.makedirs(report_dir)
@@ -124,6 +126,7 @@ def generate_report(report, report_dir):
                 f.write(f"\t\tCIRCL: {file['circl']}\n")
                 f.write(f"\t\tVirusTotal: {file['virustotal']}\n")
             f.write("\n")
+    print("# Report generated")
 
 def main():
     parser = argparse.ArgumentParser(description="Malicious File Detection Tool")
